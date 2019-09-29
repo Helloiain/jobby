a3e4795511f00897d245b02a3d37fd7d21211299
Helloiain <359556118+Helloiain@users.noreply.github.com>
2019-09-28 22:50:58 -0500

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -22,17 +21,16 @@ function compile(str, path) {
 }
 
 app.use(stylus.middleware({
-  src: __dirname + '/public',
+  src: __dirname + '/assets',
   compile: compile
 }))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'assets')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  axios.get('https://indreed.herokuapp.com/api/jobs?q=Web%20Developer')
+    .then(response => {
+      let jobsArray = []
+
+      response.data.map((job) => {
+        jobsArray.push(job)
+      })
+
+      res.render('index', {
+        jobs: jobsArray,
+        title: 'Jobby'
+      })
+    })
+    .catch(err => {
+      console.log('error')
+    })
 });
 
 module.exports = router;
