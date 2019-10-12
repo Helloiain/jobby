94f5f76f85537ee3d49330ad74d646eb92e80758
Helloiain <359556118+Helloiain@users.noreply.github.com>
2019-10-12 16:09:26 -0500

    add serverless-http

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-var createError = require('http-errors');
-var express = require('express');
-var stylus = require('stylus')
-var nib = require('nib')
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var stylus = require("stylus");
+var nib = require("nib");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var serverless = require("serverless-http");
 
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 function compile(str, path) {
   return stylus(str)
-    .set('filename', path)
-    .use(nib())
+    .set("filename", path)
+    .use(nib());
 }
 
-app.use(stylus.middleware({
-  src: __dirname + '/assets',
-  compile: compile
-}))
-app.use(logger('dev'));
+app.use(
+  stylus.middleware({
+    src: __dirname + "/assets",
+    compile: compile
+  })
+);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, "assets")));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,11 +44,12 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
+module.exports = serverless(app);
